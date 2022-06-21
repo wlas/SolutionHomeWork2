5bfc1f4f1954346f84f2923c0b18ef20f13b8fd2
SandrLite <mr.wlas@mail.ru>
2022-06-21 15:27:53 +0300

    Задание 1.1

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HomeWork1
 {
@@ -10,7 +6,49 @@ namespace HomeWork1
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите число: ");
+            if(long.TryParse(Console.ReadLine(),out long n))
+            {
+                if (Task1(n))
+                {
+                    Console.WriteLine("Простое число.");
+                }
+                else
+                {
+                    Console.WriteLine("Не простое число.");
+                }
+            }
 
-        }        
+            Console.ReadKey(true);
+        }
+        #region Task1
+        //Первое задание
+        static bool Task1(long n)
+        {
+            int i = 2;
+            int d = 0;
+            while (i < n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (d == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
     }
 }
