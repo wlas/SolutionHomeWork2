911a494118cfd60fe02fe3ce79b4817e63f1b013
SandrLite <mr.wlas@mail.ru>
2022-06-21 15:33:57 +0300

    Задание 1.2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -50,5 +50,31 @@ namespace HomeWork1
             }
         }
         #endregion
+
+        #region Task2
+        // Асимптотическую сложность функции составляет O(n^3)
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)
+                    {
+                        int y = 0;
+                        if (j != 0)
+                        {
+                            y = k / j;
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;
+                    }
+                }
+
+            }
+            return sum;
+        }
+        #endregion
     }
 }
